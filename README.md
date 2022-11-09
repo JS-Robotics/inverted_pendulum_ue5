7aea956e366f3487c28ac10f2b4b0701cb140942
Sondre <sjohnsen.valle@gmail.com>
2022-11-09 18:38:02 +0100

    End of day

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Inverted Pendulum - Unreal Engine 5
- **TODO: require more sophisticated swing up, due to rail limitations **
-
 Swing up papers:
 * https://www.researchgate.net/publication/236619208_Swing-Up_Methods_For_Inverted_Pendulum - min/max
 * https://www.researchgate.net/publication/303763786_Real-Time_Swing-up_and_Stabilization_Control_of_a_Cart-Pendulum_System_with_Constrained_Cart_Movement  
--- Source/inverted_pendulum/Private/SystemParameters.h
+++ Source/inverted_pendulum/Private/SystemParameters.h
@@ -1,10 +1,10 @@
 # pragma once
 
 struct MechanicalParamerters {
-	float MassPendulum = 0.071f;
-	float MassCart = 0.288f;
-	float LengthPendulum = 0.685f - 0.246f;
-	float InertiaPendulum = 0.0000005f;
+	float m_pendulum = 0.071f;
+	float m_cart = 0.288f;
+	float l_pendulum = 0.685f - 0.246f;
+	float I_pendulum = 0.0000005f;
 	float b_pendulum = 0.00001; // Pendulum viscous damping
 	float b_cart = 0.1f;  // Cart viscous damping
 }
